724b29d4a8ce3c58c86f66f7941a2d6f2f9006e3
KirisameVanilla <118162831+kirisamevanilla@users.noreply.github.com>
2025-09-21 21:11:38 +0800

    fix: api

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // 创建axios实例
 const apiClient = axios.create({
-  baseURL: 'http://124.221.145.135', // 后端API地址
+  baseURL: 'https://124.221.145.135:8000', // 后端API地址
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
